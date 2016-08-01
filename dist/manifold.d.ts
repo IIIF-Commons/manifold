c9a07f45f7ab30e0f50e4600a4b9c7fd4b9dfd27
edsilv <e.silverton@gmail.com>
2016-08-01 14:07:55 +0100

    bump

--- dist/manifold.d.ts
+++ dist/manifold.d.ts
@@ -1,4 +1,4 @@
-// manifold v1.0.1 https://github.com/viewdir/manifold#readme
+// manifold v1.0.2 https://github.com/viewdir/manifold#readme
 declare namespace Manifold {
     class StringValue {
         value: string;
