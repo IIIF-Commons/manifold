daa50a61da6c996bd94067f3334c2b312c3de866
edsilv <e.silverton@gmail.com>
2016-10-25 11:51:48 +0100

    merged metadata, bumped to 1.1.0

--- dist/manifold.d.ts
+++ dist/manifold.d.ts
@@ -1,4 +1,4 @@
-// manifold v1.0.5 https://github.com/viewdir/manifold#readme
+// manifold v1.1.0 https://github.com/viewdir/manifold#readme
 declare namespace Manifold {
     class StringValue {
         value: string;
