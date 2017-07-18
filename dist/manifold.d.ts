5620d576916ad2cc911e23f638749252a88232ed
edsilv <e.silverton@gmail.com>
2017-07-18 17:46:49 +0100

    update manifesto

--- dist/manifold.d.ts
+++ dist/manifold.d.ts
@@ -1,4 +1,4 @@
-// manifold v1.1.12 https://github.com/viewdir/manifold#readme
+// manifold v1.2.0 https://github.com/viewdir/manifold#readme
 /// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
 interface Window {
     manifestCallback: any;
