4e6c64949ff2043fca9a048a41f43aacdd045273
edsilv <e.silverton@gmail.com>
2018-03-07 14:33:55 +0000

    merged getvalue

--- dist/manifold.d.ts
+++ dist/manifold.d.ts
@@ -1,4 +1,4 @@
-// @iiif/manifold v1.2.20 https://github.com/iiif-commons/manifold#readme
+// @iiif/manifold v1.2.21 https://github.com/iiif-commons/manifold#readme
 interface Window {
     manifestCallback: any;
 }
