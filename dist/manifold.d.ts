e5b749b5db73caae26fd018f52fa8d6b247e9244
edsilv <e.silverton@gmail.com>
2018-08-11 16:45:38 +0100

    check annotation bodies for image service

--- dist/manifold.d.ts
+++ dist/manifold.d.ts
@@ -1,4 +1,4 @@
-// @iiif/manifold v1.2.29 https://github.com/iiif-commons/manifold#readme
+// @iiif/manifold v1.2.30 https://github.com/iiif-commons/manifold#readme
 interface Window {
     manifestCallback: any;
 }
